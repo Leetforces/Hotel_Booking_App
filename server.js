cb7cb3a9cab79787a87d36aafd6b60c72fc2fb99
Manish Kumar <manishju22@gmail.com>
2021-05-27 20:23:52 +0530

    deploy settings

--- server.js
+++ server.js
@@ -15,7 +15,23 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+
+
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
+if(process.env.NODE_ENV==='production'){
+    //serve static files
+    app.use(express.static("client/build"));
+
+    const path= require('path');
+
+    //for any other request that is not in routes
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}    
+
+
 
 const port = process.env.PORT || 5000;
 
